0965982af025a14804e445a03cccd2cb78c93bd5
Manuel Aschwanden <manuel_aschwanden@gmx.ch>
2023-04-30 01:00:36 +0200

    added lots of dog sounds

--- tichu-app/src/App.tsx
+++ tichu-app/src/App.tsx
@@ -1,9 +1,13 @@
 import bomb1File from "./sounds/bombs/LaBomba.mp3";
 import bomb2File from "./sounds/bombs/Boombastic.mp3";
-import dog1File from "./sounds/dogs/DogsOut.mp3";
+import dog1File from "./sounds/dogs/dog1.mp3";
+import dog2File from "./sounds/dogs/dog2.mp3";
+import dog3File from "./sounds/dogs/dog3.mp3";
+import dog4File from "./sounds/dogs/dog4.mp3";
+import dog5File from "./sounds/dogs/DogsOut.mp3";
 
 const bombSounds = [bomb1File, bomb2File];
-const dogSounds = [dog1File];
+const dogSounds = [dog1File, dog2File, dog3File, dog4File, dog5File];
 let audio = new Audio(undefined);
 
 const playBomb = () => {
