cf3f2f53610557c58e010173c5ba2d12fa8fd285
Manuel Aschwanden <manuel_aschwanden@gmx.ch>
2023-04-30 00:03:37 +0200

    randomly pick song for bomb or dog on button click

--- tichu-app/src/App.tsx
+++ tichu-app/src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import bomb1File from "./sounds/bombs/LaBomba.mp3";
+import bomb2File from "./sounds/bombs/Boombastic.mp3";
+import dog1File from "./sounds/dogs/DogsOut.mp3";
 
-function App() {
+const bombSounds = [bomb1File, bomb2File];
+const dogSounds = [dog1File];
+let audio = new Audio(undefined);
+
+const playBomb = () => {
+  stopSound();
+  const randomIndex = getRandomIndex(bombSounds.length);
+  audio = new Audio(bombSounds[randomIndex]);
+  audio.play();
+};
+
+const playDog = () => {
+  stopSound();
+  const randomIndex = getRandomIndex(dogSounds.length);
+  audio = new Audio(dogSounds[randomIndex]);
+  audio.play();
+};
+
+const stopSound = () => {
+  audio.pause();
+  audio.currentTime = 0;
+};
+
+const getRandomIndex = (maxIndex: number) => {
+  return Math.floor(Math.random() * maxIndex);
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <div>
+        <button onClick={playBomb}>Bombe</button>
+      </div>
+      <div>
+        <button onClick={playDog}>Hund</button>
+      </div>
+      <div>
+        <button onClick={stopSound}>Ruäh!!!</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
